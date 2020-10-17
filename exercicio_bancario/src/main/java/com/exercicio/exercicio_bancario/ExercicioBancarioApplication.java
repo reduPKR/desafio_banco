d819827ceb67fcf4521b09052c3cdcc52a0e335a
redupkr <reduardo1994@hotmail.com>
2020-10-17 00:33:30 -0300

    Iniciando desafio

---
+++ exercicio_bancario/src/main/java/com/exercicio/exercicio_bancario/ExercicioBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.exercicio_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioBancarioApplication.class, args);
+	}
+
+}
---
+++ exercicio_bancario/src/test/java/com/exercicio/exercicio_bancario/ExercicioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.exercicio_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
